45e7df27c3845368d4a27fbb2136c110714a9a9d
Dominick Harper <dominickdustinjames@gmail.com>
2024-06-18 01:13:25 -0600

    thumbnails

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../Projects/Projects.css";
+import news from "../../images/news.png";
+import portfolio from "../../images/portfolio.png";
+import usaapp from "../../images/usapp.png";
 
 const Projects = () => {
   const projects = [
@@ -7,22 +10,25 @@ const Projects = () => {
       id: 1,
       title: "Around the US",
       githubLink: "https://dominickdj.github.io/se_project_aroundtheus/",
+      thumbnail: usaapp,
     },
     {
       id: 2,
-      title: "News Seach App",
+      title: "News Search App",
       githubLink: "https://dominickdj.github.io/Dominick_Frontend/",
+      thumbnail: news,
     },
     {
       id: 3,
       title: "Portfolio Code",
       githubLink: "https://github.com/DominickDJ/Portfolio",
+      thumbnail: portfolio,
     },
   ];
 
   return (
     <section id="projects" className="center">
-      <h1 className="project__title"> PROJECTS</h1>
+      <h1 className="project__title">PROJECTS</h1>
       <div className="project-thumbnails">
         {projects.map((project) => (
           <a
@@ -32,7 +38,12 @@ const Projects = () => {
             rel="noopener noreferrer"
             className="project-link"
           >
-            <div className="project-thumbnail"></div>
+            <div
+              className="project-thumbnail"
+              style={{
+                backgroundImage: `url(${project.thumbnail})`,
+              }}
+            ></div>
             <h3>{project.title}</h3>
           </a>
         ))}
